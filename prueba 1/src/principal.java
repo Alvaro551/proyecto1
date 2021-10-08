6137b8a07748fe6403d80f6b9c409c4e4c43299e
alu1daw <alu1daw@DESKTOP-1DUFOL5>
2021-10-08 13:58:38 +0200

    Validacion del tercer cambio

--- prueba 1/src/principal.java
+++ prueba 1/src/principal.java
@@ -4,6 +4,7 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola mundo de java");
+System.out.println("Hola mundo de java2");
 	}
 
 }
