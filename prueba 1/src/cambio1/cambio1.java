4c77bb13c7233dc9e8a8e51566ee9835bae954fc
alu1daw <alu1daw@DESKTOP-1DUFOL5>
2021-10-15 13:57:17 +0200

    Commit del cambio 1 en el paquete cambio 1

---
+++ prueba 1/src/cambio1/cambio1.java
@@ -0,0 +1,16 @@
+package cambio1;
+
+public class cambio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo java-git");
+		
+		
+		
+		
+
+	}
+
+}
